2c212175bc61f02ae514affa6c01b70f6d993759
Arihant Aggarwal <arihant_a@ee.iitr.ac.in>
2024-12-27 19:43:54 +0530

    1 before dvc

---
+++ mycode.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+data={
+    'name':['Tom','Jerry','Mickey'],
+    'age':[20,21,22],
+    'city':['New York','Los Angeles','Orlando']
+}
+df=pd.DataFrame(data)
+
+
+
+
+
+
+data_dir='data'
+os.makedirs(data_dir,exist_ok=True)
+
+file_path=os.path.join(data_dir,'sample_data.csv')
+df.to_csv(file_path,index=False)
+
+print(f"csv file saved to {file_path}")
